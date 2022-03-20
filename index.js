e46e7d9b4ea9127d1596aab36c7aadfd4890beba
Daudxu <875126243@qq.com>
2022-03-20 10:41:23 +0000

    Deploying to gh-pages from  @ da1a752952a471cc13cbbdfd352c291a45b65463 🚀

--- index.js
+++
@@ -1,6 +0,0 @@
-// index.js
-if (process.env.NODE_ENV === 'production') { // 通过环境变量来决定入口文件
-  module.exports = require('./lib/dapp-wallet-modal.umd.min.js')
-} else {
-  module.exports = require('./lib/dapp-wallet-modal.umd.js')
-}
