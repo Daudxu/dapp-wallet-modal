0aaef6e8bf8d3aaca04d14b9c6eadb7edd0be719
xudan <875126243@qq.com>
2022-03-29 00:22:31 +0800

    update

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div class="hero min-h-screen" style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
   <div class="hero-overlay bg-opacity-60"></div>
   <div class="hero-content text-center text-neutral-content">
