a7ebc7412d8e793e162725a9649a5c6822994e89
xudan <875126243@qq.com>
2022-03-29 00:30:11 +0800

    Modal

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -24,13 +24,33 @@
         </div>
         <div class="divider"></div> 
         <div class="btu"> 
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-success">Success</button>
+            <!-- The button to open modal -->
+            <label for="my-modal-1" class="btn btn-success">open modal</label>
+
+            <!-- Put this part before </body> tag -->
+            <input type="checkbox" id="my-modal-1" class="modal-toggle btn btn-success">
+            <div class="modal">
+              <div class="modal-box relative">
+                <label for="my-modal-1" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold" style="color:#000">Congratulations </h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+              </div>
+            </div>
+            <!-- The button to open modal -->
+            <label for="my-modal-3" class="btn btn-success">open modal</label>
+
+            <!-- Put this part before </body> tag -->
+            <input type="checkbox" id="my-modal-3" class="modal-toggle btn btn-success">
+            <div class="modal">
+              <div class="modal-box relative">
+                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold" style="color:#000"> random Interner user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+              </div>
+            </div>
+
         </div>
+
       </div>
     </div>
   </div>
@@ -267,7 +287,7 @@ export default {
 </script>
 <style scoped>
 
-.btu button {
+.btu button,label {
   margin-left: 10px;
   margin-bottom: 10px;
 }
