e623ed7d0206b51bdd4af7abb85caa717ca9bfcc
xudan <xudan@apowo.com>
2022-03-29 10:56:00 +0800

    demo

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -1,60 +1,115 @@
 <template>
 
-<div class="hero min-h-screen" style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
-  <div class="hero-overlay bg-opacity-60"></div>
-  <div class="hero-content text-center text-neutral-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Dapp Wallet Modal</h1>
-      <p class="mb-5">Dapp ETH Wallet Modal plugin.</p>
-      <!-- <button class="btn btn-primary">Get Started</button> -->
-      <div v-show="provider === ''">
-        <button class="btn btn-success"  @click="handleClickConnect">Connect</button>
-      </div>
-      <div v-show="provider" >
-        <button class="btn btn-primary"  @click="handleClickDisconnect">disconnect</button>
-        <div class="divider"></div> 
-        <div class="btu"> 
-          <button class="btn btn-active">getBalanceOf</button>
-          <button class="btn btn-active">getTotalSupply</button>
-          <button class="btn btn-active">getAllowance</button>
-          <button class="btn btn-active">mint</button>
-          <button class="btn btn-active">transfer</button>
-          <button class="btn btn-active">transferFrom</button>
-          <button class="btn btn-active">approve</button>
+  <div class="hero min-h-screen"
+       style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Dapp Wallet Modal</h1>
+        <p class="mb-5">Dapp ETH Wallet Modal plugin.</p>
+        <!-- <button class="btn btn-primary">Get Started</button> -->
+        <div v-show="provider === ''">
+          <button class="btn btn-success"
+                  @click="handleClickConnect">Connect</button>
         </div>
-        <div class="divider"></div> 
-        <div class="btu"> 
+        <div v-show="provider">
+          <button class="btn btn-primary"
+                  @click="handleClickDisconnect">disconnect</button>
+          <div class="divider"></div>
+          <div class="btu">
             <!-- The button to open modal -->
-            <label for="my-modal-1" class="btn btn-success">open modal</label>
+            <label for="my-modal-1"
+                   class="btn btn-success">get BalanceOf</label>
 
             <!-- Put this part before </body> tag -->
-            <input type="checkbox" id="my-modal-1" class="modal-toggle btn btn-success">
+            <input type="checkbox"
+                   id="my-modal-1"
+                   class="modal-toggle btn btn-success">
             <div class="modal">
               <div class="modal-box relative">
-                <label for="my-modal-1" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                <h3 class="text-lg font-bold" style="color:#000">Congratulations </h3>
+                <label for="my-modal-1"
+                       class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold"
+                    style="color:#000">Get BalanceOf </h3>
+                <p class="py-4">
+                  {{balanceOfCount}}
+                </p>
+                <p class="py-4">
+                  <input type="text"
+                         v-model="ownerAdress"
+                         placeholder="owner address"
+                         class="input w-full max-w-xs">
+                </p>
+                <p class="py-4">
+                  <button class="btn btn-active"
+                          @click="handleClickGetBalanceOf">getBalanceOf</button>
+                </p>
+              </div>
+            </div>
+
+            <!-- The button to open modal -->
+            <label for="my-modal-2"
+                   class="btn btn-success">Get TotalSupply</label>
+
+            <!-- Put this part before </body> tag -->
+            <input type="checkbox"
+                   id="my-modal-2"
+                   class="modal-toggle btn btn-success">
+            <div class="modal">
+              <div class="modal-box relative">
+                <label for="my-modal-2"
+                       class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold"
+                    style="color:#000">Get TotalSupply </h3>
+                <p class="py-4">
+                  {{totalSupplyCount}}
+                </p>
+                <p class="py-4">
+                  <input type="text"
+                         v-model="contractAdress"
+                         placeholder="contract address"
+                         class="input w-full max-w-xs">
+                </p>
+                <p class="py-4">
+                  <button class="btn btn-active"
+                          @click="handleClickGetTotalSupply">getTotalSupply</button>
+                </p>
+              </div>
+            </div>
+
+            <!-- <button class="btn btn-active">getAllowance</button>
+            <button class="btn btn-active">mint</button>
+            <button class="btn btn-active">transfer</button>
+            <button class="btn btn-active">transferFrom</button>
+            <button class="btn btn-active">approve</button> -->
+          </div>
+          <div class="divider"></div>
+
+          <div class="btu">
+            <!-- The button to open modal -->
+            <label for="my-modal-3"
+                   class="btn btn-success">open modal</label>
+
+            <!-- Put this part before </body> tag -->
+            <input type="checkbox"
+                   id="my-modal-3"
+                   class="modal-toggle btn btn-success">
+            <div class="modal">
+              <div class="modal-box relative">
+                <label for="my-modal-3"
+                       class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold"
+                    style="color:#000"> random Interner user!</h3>
                 <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
               </div>
             </div>
-            <!-- The button to open modal -->
-            <label for="my-modal-3" class="btn btn-success">open modal</label>
 
-            <!-- Put this part before </body> tag -->
-            <input type="checkbox" id="my-modal-3" class="modal-toggle btn btn-success">
-            <div class="modal">
-              <div class="modal-box relative">
-                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                <h3 class="text-lg font-bold" style="color:#000"> random Interner user!</h3>
-                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-              </div>
-            </div>
+          </div>
 
         </div>
-
       </div>
     </div>
   </div>
-</div>
 
 </template>
 
@@ -119,6 +174,11 @@ export default {
   data () {
     return {
       baseModel: '',
+      ownerAdress: '0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166',
+      contractAdress: '0x8D0CD152eDCE6D3468884AD1ade809ce6A02e53f',
+      netId: 4,
+      balanceOfCount: 0,
+      totalSupplyCount: 0,
       provider: '',
       providerOptions: {
         logo: '',
@@ -241,15 +301,13 @@ export default {
     var _this = this
     if (walletType && typeof (walletType) !== 'undefined') {
       setTimeout(function () {
-        console.log('lai le')
         _this.handleClickConnect()
-      }, 3000)
+      }, 1000)
     }
   },
   methods: {
     async handleClickConnect () {
       var provider = await this.baseModel.connect()
-      console.log('provider', provider)
       if (provider) {
         this.provider = provider
       }
@@ -262,11 +320,9 @@ export default {
       // https://rinkeby.etherscan.io/address/0x8d0cd152edce6d3468884ad1ade809ce6a02e53f
       //  console.log(CHAIN_DATA_LIST)
       // var b = new web3Model(ethers)
-      // console.log(b.run())
-      // console.log(Web3.utils.isAddress("0xdDC78e537D177D5e51598B02D04Ac8b5578Ed156"))
-      var web3ModelObj = await new web3Model(this.provider, Web3, 4, "0x8D0CD152eDCE6D3468884AD1ade809ce6A02e53f", abi)
+      var web3ModelObj = await new web3Model(this.provider, Web3, this.netId, this.contractAdress, abi)
+      this.totalSupplyCount = await web3ModelObj.getTotalSupply()
 
-      console.log(await web3ModelObj.getTotalSupply())
 
       // console.log(await web3ModelObj.getBalanceOf('0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166'))
 
@@ -281,15 +337,29 @@ export default {
       // console.log(await web3ModelObj.transferFrom("0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166", "0xdDC78e537D177D5e51598B02D04Ac8b5578Ed156", 5))
 
 
+    },
+    async handleClickGetBalanceOf () {
+      var web3ModelObj = await new web3Model(this.provider, Web3, this.netId, this.contractAdress, abi)
+      this.balanceOfCount = await web3ModelObj.getBalanceOf(this.ownerAdress)
+    },
+    async handleClickGetTotalSupply () {
+      var web3ModelObj = await new web3Model(this.provider, Web3, this.netId, this.contractAdress, abi)
+      this.totalSupplyCount = await web3ModelObj.getTotalSupply()
     }
   }
 }
 </script>
 <style scoped>
-
-.btu button,label {
+.btu button,
+label {
   margin-left: 10px;
   margin-bottom: 10px;
 }
-
+.py-4 input {
+  border: 1px solid #000;
+  color: #000;
+}
+.modal-box {
+  color: #000;
+}
 </style>
